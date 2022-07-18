1fff4c92192c38191a588ea0a0ab787d44e8162e
Ranulfo Bezerra <bezerra.ranulfo@rm.is.tohoku.ac.jp>
2022-07-18 15:24:30 +0900

    init

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# cv
+# Curriculum Vitae
+
+This is my CV in LaTeX based on the [moderncv](https://ctan.org/pkg/moderncv) class following [gkthiruvathukal's example](https://github.com/gkthiruvathukal/cv).
+
+You can view/download the latest version [here](https://github.com/ranulfobezerra/CV/releases/latest/download/ranulfo-cv.pdf).
---
+++ build-cv.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [ -d ~/.linuxbrew ]; then
+  PATH=/usr/bin:$PATH
+fi
+
+NAME=ranulfo
+MAIN=${NAME}-cv
+DOMAIN=${NAME}.netlify.app
+
+# Each of my BibTeX entries is kept in a separate file and assembled into one (for each type of publication).
+# Makes it easier to keep up-to-date.
+# Every entry in the .bib MUST have a year to sort properly. (We don't rigidly sort by other fields, month, day, since these don't always appear).
+
+echo "Collating bibliography"
+pushd bibs >& /dev/null
+bash gather.sh
+popd >& /dev/null
+sleep 5
+
+echo "Obtaining Google Scholar Data"
+python3 tools/scholarly-metrics.py --name "Ranulfo Bezerra"
+python3 tools/github-commits.py  --first-year 2017 --last-year 2022 --username ranulfobezerra --modern-cv
+sleep 5
+
+latexmk -output-directory="./build" -C -pdf ${MAIN}.tex
+latexmk -output-directory="./build" -pdf ${MAIN}.tex
+
+if [ -f build/${MAIN}.pdf ]; then
+  if [ -d ../${DOMAIN} ]; then
+     cat bibliography/*.bib > ../${DOMAIN}/_bibliography/papers.bib
+     echo "Copied latest bibliography to website folder"
+  fi 
+fi
